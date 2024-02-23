2f01b115bc5f21f9cdcadddc5323fe378a2c9125
Yash Singhal <yashsinghal9886@gmail.com>
2024-02-23 23:31:30 +0530

    Used speech recognition hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import 'regenerator-runtime/runtime';
 
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 const App = () => {
+  const startListening=()=>SpeechRecognition.startListening({ continuous: true })
+  const stopListening=()=>SpeechRecognition.stopListening()
+
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
+  
   return (
-    <div>App</div>
+    <div className='bg-black h-screen '>
+      <h1 className='text-white font-bold  text-4xl flex justify-center p-6'>
+        Speech to Text Converter</h1>
+        <br />
+        <p className='text-white flex justify-center text-lg font-mono'>
+          Convert and copy the speech you speak in the form of text.</p>
+
+
+          <div className='text-white'>
+            {transcript}
+          </div>
+
+          <div className='text-white flex justify-center items-center m-6 p-4 '>
+            <button>Copy</button>
+            <button onClick={startListening}>Start Listening</button>
+            <button onClick={stopListening}>Stop Listening</button>
+
+            </div>
+    </div>
   )
 }
 
