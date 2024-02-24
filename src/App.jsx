405258357a1e6f097dc5edb03db34fecc2b643e3
Yash Singhal <yashsinghal9886@gmail.com>
2024-02-24 20:39:57 +0530

    UI modified

--- src/App.jsx
+++ src/App.jsx
@@ -15,22 +15,22 @@ const App = () => {
   
   return (
     <div className='bg-black h-screen '>
-      <h1 className='text-white font-bold  text-4xl flex justify-center p-6'>
+      <h1 className='text-white font-bold  text-4xl flex justify-center pt-6'>
         Speech to Text Converter</h1>
         <br />
-        <p className='text-white flex justify-center text-lg font-mono'>
+        <p className='text-gray-400 flex justify-center text-lg font-mono '>
           Convert and copy the speech you speak in the form of text.</p>
 
-          <div className='w-4/12 mx-auto'>
-          <div className='text-white flex  justify-center  bg-blue-800 '>
-            {transcript}
+          <div className='w-5/12  mx-auto'>
+          <div className='text-white flex  justify-center   mt-10 '>
+            <textarea  placeholder='Start Speaking...' cols="70" rows="10" value={transcript} className='bg-gray-400 bg-opacity-50 text-white rounded-md'></textarea>
           </div>
           </div>
 
-          <div className='text-white flex justify-center items-center m-6 p-4 '>
-            <button>Copy</button>
-            <button onClick={startListening}>Start Listening</button>
-            <button onClick={stopListening}>Stop Listening</button>
+          <div className='text-white flex justify-center items-center m-6 p-4  font-bold text-xl'>
+            <button className='mx-8 bg-blue-500 p-2 px-2 rounded-lg'>Copy</button>
+            <button className='mx-8 bg-blue-500 p-2 px-2 rounded-lg' onClick={startListening}>Start Listening</button>
+            <button className='mx-8 bg-blue-500 p-2 px-2 rounded-lg' onClick={stopListening}>Stop Listening</button>
 
             </div>
     </div>
